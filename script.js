ef687779eb80f98325a5364c69b3791ee79eb05a
ashan <indrajee79@gmail.com>
2023-02-02 08:40:32 +0530

    js stuff

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-import get from './utils/getElement.js';
-import getUser from './utils/fetchUser.js';
-import displayUser from './utils/displayUser.js'
+import get from "./utils/getElement.js";
+import getUser from "./utils/fetchUser.js";
+import displayUser from "./utils/displayUser.js";
 
-const btn = get('.btn');
+const btn = get(".btn");
 
-const showUser = async () = {
-    // get user from api
-    const person = await getUser();
-    displayUser(person);
+const showUser = async () => {
+  // get user from api
+  const person = await getUser();
+  displayUser(person);
 
-    //display user
+  // display user
 };
 
-window.addEventListener('DOMContentLoaded', showUser);
-btn.addEventListener('click', showUser);
+window.addEventListener("DOMContentLoaded", showUser);
+btn.addEventListener("click", showUser);
